a67a6a1138fc8b688bf8d06defc7f9298115f224
SK Wang <shoukaiw@opay.team>
2020-04-21 18:59:08 +0800

    Add leetcode_316 demo

---
+++ AlgorithmExercisesDemo/06_去除重复字母_Leetcode_316/main.c
@@ -0,0 +1,34 @@
+//
+//  main.c
+//  06_去除重复字母_Leetcode_316
+//
+//  Created by SK_Wang on 2020/4/21.
+//  Copyright © 2020 SK_Wang. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ 316. 去除重复字母（https://leetcode-cn.com/problems/remove-duplicate-letters）
+ 给你一个仅包含小写字母的字符串，请你去除字符串中重复的字母，使得每个字母只出现一次。需保证返回结果的字典序最小（要求不能打乱其他字符的相对位置）。
+
+ 示例 1:
+ 输入: "bcabc"
+ 输出: "abc"
+ 示例 2:
+ 输入: "cbacdcbc"
+ 输出: "acdb"
+ */
+
+char * removeDuplicateLetters(char * s) {
+    
+
+    return NULL;
+}
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
